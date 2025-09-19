ab3a76ecaa7f207c533eb55b0b98b0dbb48d51ba
sajils5726-max <sajilsajeev5726@gmail.com>
2025-09-19 14:26:38 +0530

    completed

---
+++ Collection.py
@@ -0,0 +1,18 @@
+thislist=["apple","orange","banana"]
+print(len(thislist))
+# del thislist
+# print(thislist)
+# thislist=["apple","orange","banana"]
+# for x in thislist:
+#     print(x)
+#     thislist.sort()
+#     thislist.sort(reverse=True)
+#     thislist=["apple","orange","banana"]
+#    mylist=thislist.copy()
+#     print(mylist)
+
+thistuple=("apple","banana","cherry")
+y=list(thistuple)
+y.append("orange")
+thistuple=tuple(y)
+    
---
+++ color list1&2.py
@@ -0,0 +1,8 @@
+colorlist1={'Red','Yellow','purple','Blue'}
+colorlist2={'Pink','Green','Lavander','violet'}
+
+print(colorlist1)
+print(colorlist2)
+
+colorlist3=colorlist1.difference(colorlist2)
+print(colorlist3)
