0f2945ae12c5b9e4109c5be8b3c51360dae6e7b4
SAJIL S <sajilsajeev5726@gmail.com>
2025-10-10 10:52:27 +0530

    Collection.py

--- Collection.py
+++ Collection.py
@@ -1,18 +1,9 @@
 thislist=["apple","orange","banana"]
 print(len(thislist))
-# del thislist
-# print(thislist)
-# thislist=["apple","orange","banana"]
-# for x in thislist:
-#     print(x)
-#     thislist.sort()
-#     thislist.sort(reverse=True)
-#     thislist=["apple","orange","banana"]
-#    mylist=thislist.copy()
-#     print(mylist)
 
 thistuple=("apple","banana","cherry")
 y=list(thistuple)
 y.append("orange")
 thistuple=tuple(y)
+
     
