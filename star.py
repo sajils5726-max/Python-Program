52563f9c04af042d00b62c2378735970507a6f88
SAJIL S <sajilsajeev5726@gmail.com>
2025-10-15 11:32:37 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,25 @@
+#for i in range(6):
+   # print("*"*6)
+
+
+
+#n=int(input("Enter the limit:"))
+#for i in range(1,n+1):
+    #print(i*"*") 
+
+
+
+#n=int(input("Enter the limit:"))
+#for i in range(n,0,-1):
+    #print(i*"*")
+
+
+
+n=int(input("Enter the limit:"))
+for i in range(1,n+1):
+    print(i*"*") 
+for i in range(n,0,-1):
+    print(i*"*")
+
+
+
